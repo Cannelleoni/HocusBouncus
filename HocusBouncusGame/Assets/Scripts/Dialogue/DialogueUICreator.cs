7d6e951643392ad62187ca9adda042d6ed0edf57
Leon Portius <Pluto-is-not-too-small@gmx.de>
2019-10-12 02:20:22 +0200

    made Dialogue UI prefab
    first design

---
+++ HocusBouncusGame/Assets/Scripts/Dialogue/DialogueUICreator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueUICreator : MonoBehaviour
+{
+    /* The needed elements are: 
+     * speaker1Portrait
+     * speaker2Portrait
+     * 
+     * speaker1Label
+     * speaker1LabelBox
+     * speaker2Label
+     * speaker2LabelBox
+     * 
+     * dialogueBox
+     * text
+     * 
+     * continueButton with label
+     * 
+     * 
+     */
+
+    private void Start()
+    {
+        
+
+
+
+    }
+
+
+
+
+
+}
