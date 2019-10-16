e907bba0346d022f934533d09a9e6bef36fb0b97
Tina <tina.kiessling@t-online.de>
2019-10-16 23:14:12 +0200

    Camera Follow added, Test Level Design
    Camera is now following the player
    Collider is attached to player object and sample tiles
    little test level added to test Movement (folder from asset store)

---
+++ HocusBouncusGame/Assets/Scripts/Movement/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Player as game object that can be referenced.
+    [SerializeField] GameObject player;
+    // Saves the players current position.
+    [SerializeField] Vector3 playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+        transform.position = playerPosition;
+    }
+}
